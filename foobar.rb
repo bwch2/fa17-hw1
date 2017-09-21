ab07352e4f7c07f91656ee013d25559827e48e2e
Benjamin <benjaminho@berkeley.edu>
2017-09-20 19:48:25 -0700

    hw1 submission

--- foobar.rb
+++ foobar.rb
@@ -3,6 +3,10 @@ class Foobar
   def self.baz(a)
     # Class method
     # Call with `Foobar.baz`
+    
+    
+    
+    
   end
 end
 
--- hw1.rb
+++ hw1.rb
@@ -1,15 +1,26 @@
 def squared_sum(a, b)
   # Q1 CODE HERE
+  x = a+b
+  return x*x
 
 end
 
 def sort_array_plus_one(a)
   # Q2 CODE HERE
+  
+  a.sort
+  
+  a.each { |a| print a+=1, " " }
+  
+  return a
+
 
 end
 
 def combine_name(first_name, last_name)
   # Q3 CODE HERE
+  
+  print first_name + '' + last_name
 
 end
 
@@ -18,3 +29,6 @@ def blockin_time(a)
   require './foobar'
   Foobar.baz a
 end
+
+
+
